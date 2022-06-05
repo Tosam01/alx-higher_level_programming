a707dfd9bd12eae94e6c2fdc4af71d521bebe067
Tosam01 <tosam0001@gmail.com>
2022-06-05 22:39:19 +0000

    tk3

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, elment):
+def replace_in_list(my_list, idx, element):
     listlength = len(my_list) - 1
     if (idx < 0 or idx > listlength):
         return (my_list)
